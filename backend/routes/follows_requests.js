7d5fa18fe2842c2c4f9a69208eb806eb4be06839
Rellow09-code <2769364@students.wits.ac.za>
2025-08-30 03:32:23 +0200

    update: comments,friend & group functionality

--- backend/routes/follows_requests.js
+++ backend/routes/follows_requests.js
@@ -5,6 +5,88 @@ const {verifyToken, errorClass} = require('../middleware/tools');
 const { User, Follows } = require('../models');
 const { sequelize } = require('../config/database');
 
+/**
+ * @swagger
+ * /api/v1/friends/request:
+ *   post:
+ *     summary: Send a friend request to another user
+ *     tags: [Friends]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - token
+ *               - id
+ *               - username
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 description: Firebase token of the user
+ *               id:
+ *                 type: string
+ *                 format: uuid
+ *                 description: ID of the user sending the request
+ *               username:
+ *                 type: string
+ *                 description: Username of the user to send a friend request to
+ *     responses:
+ *       200:
+ *         description: Friend request sent successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: friend request sent successfully
+ *       400:
+ *         description: Missing required information or friend request already exists
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Insufficient information provided
+ *       401:
+ *         description: Invalid token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Invalid Token
+ *       404:
+ *         description: User does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User does not exist
+ *       403:
+ *         description: duplicate request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: request already sent
+ *       500:
+ *         description: Server error
+ */
+
 //friend requests
 router.post('/request',async(req,res)=>{
     try{
@@ -14,18 +96,18 @@ router.post('/request',async(req,res)=>{
             return errorClass.insufficientInfo(res);
         }
         if (!verifyToken.fireBaseToken(token,id)){
-            return errorClass.errorRes('Invalid Token',res);
+            return errorClass.errorRes('Invalid Token',res,401);
         }
         
 
         const friend =await User.findOne({where:{username:username}})
         if (!friend){
             console.log(friend);
-            return errorClass.errorRes('User does not exist',res);
+            return errorClass.errorRes('User does not exist',res,404);
         }
         const existance = await Follows_requests.findOne({where:{follower_id:id,followee_id:friend.id}});
         if (existance){
-            return errorClass.errorRes('Friend request already sent',res);
+            return errorClass.errorRes('Friend request already sent',res,403);
         }
         await Follows_requests.create({
             follower_id:id,
@@ -41,6 +123,83 @@ router.post('/request',async(req,res)=>{
 })
 
 //friend request response
+/**
+ * @swagger
+ * /api/v1/friends/request/response:
+ *   post:
+ *     summary: Respond to a friend request (accept or reject)
+ *     tags: [Friends]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - token
+ *               - id
+ *               - requestID
+ *               - response
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 description: Firebase token of the user
+ *               id:
+ *                 type: string
+ *                 format: uuid
+ *                 description: ID of the user responding to the request
+ *               requestID:
+ *                 type: integer
+ *                 description: ID of the friend request
+ *               response:
+ *                 type: string
+ *                 enum: [accept, reject]
+ *                 description: Response to the friend request
+ *     responses:
+ *       200:
+ *         description: Friend request accepted or rejected
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: You are now friends
+ *       400:
+ *         description: Missing required information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Insufficient information provided
+ *       401:
+ *         description: Invalid token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Invalid Token
+ *       404:
+ *         description: Friend request does not exist
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Friend request does not exist
+ *       500:
+ *         description: Server error
+ */
+
 
 router.post('/request/response', async (req, res) => {
     try {
@@ -50,12 +209,12 @@ router.post('/request/response', async (req, res) => {
             return errorClass.insufficientInfo(res);
         }
         if (!verifyToken.fireBaseToken(token, id)) {
-            return errorClass.errorRes('Invalid Token', res);
+            return errorClass.errorRes('Invalid Token', res,401);
         }
         
         const request = await Follows_requests.findOne({ where: { id: requestID } });
         if (!request) {
-            return errorClass.errorRes('Friend request does not exist', res);
+            return errorClass.errorRes('Friend request does not exist', res,404);
         }
 
         if (response !== 'accept') {

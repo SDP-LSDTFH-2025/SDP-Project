bbc59f78569680a29e8c65fdd88e0615df85cdcc
Lebo-Kharafu <Kharafu23@gmail.com>
2025-08-30 21:43:02 +0200

    feat: Added getCourseEnrolls in userCourses.js

--- frontend/src/functions/userCourses.js
+++ frontend/src/functions/userCourses.js
@@ -20,3 +20,26 @@ export async function getUsersCourses(userId) {
     throw error; 
   }
 }
+
+export async function getCourseEnrolls(courseId) {
+  try {
+    const SERVER = import.meta.env.VITE_PROD_SERVER || import.meta.env.VITE_DEV_SERVER;
+    let res = await fetch(`${SERVER}/api/v1/user-courses/course/${courseId}` , {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    let users = await res.json();
+
+    if (users.success) {
+      console.log("API Response:", users);
+      return users["data"];
+    } else {
+      console.error(`Failed to get users in course: ${users.message || "Unknown error"}`);
+      throw new Error(users.message || "Failed to fetch user's in course");
+    }
+  } catch (error) {
+    console.error("Courses-enrolls error:", error);
+    throw error; 
+  }
+}

06d0c8a800628701031400f844b8b21d4dd1a7c1
Lebo-Kharafu <Kharafu23@gmail.com>
2025-08-30 20:54:48 +0200

    feat: Added Function folder to create reusable functions for calling apis update: updated the Home.jsx to test getAllUsers function in users.js file

---
+++ frontend/src/functions/users.js
@@ -0,0 +1,23 @@
+// users.js
+export async function getAllUsers() {
+  try {
+    const SERVER = import.meta.env.VITE_PROD_SERVER || import.meta.env.VITE_DEV_SERVER;
+    let res = await fetch(`${SERVER}/api/v1/users`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    let users = await res.json();
+
+    if (users.success) {
+      console.log("API Response:", users);
+      return users["data"];
+    } else {
+      console.error(`Failed to get users: ${users.message || "Unknown error"}`);
+      throw new Error(users.message || "Failed to fetch users");
+    }
+  } catch (error) {
+    console.error("Users error:", error);
+    throw error; 
+  }
+}

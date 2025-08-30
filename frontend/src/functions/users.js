ad73a62bf7593bcce243f84b1cb12bc88228a414
Lebo-Kharafu <Kharafu23@gmail.com>
2025-08-30 21:09:31 +0200

    feat: Added getUser to user.js To fetch a specific user

--- frontend/src/functions/users.js
+++ frontend/src/functions/users.js
@@ -1,4 +1,3 @@
-// users.js
 export async function getAllUsers() {
   try {
     const SERVER = import.meta.env.VITE_PROD_SERVER || import.meta.env.VITE_DEV_SERVER;
@@ -18,6 +17,29 @@ export async function getAllUsers() {
     }
   } catch (error) {
     console.error("Users error:", error);
+    throw error; 
+  }
+}
+
+export async function getUser(userId) {
+  try {
+    const SERVER = import.meta.env.VITE_PROD_SERVER || import.meta.env.VITE_DEV_SERVER;
+    let res = await fetch(`${SERVER}/api/v1/users/${userId}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    let user = await res.json();
+
+    if (user.success) {
+      console.log("API Response:", user);
+      return user["data"];
+    } else {
+      console.error(`Failed to get user${userId}: ${user.message || "Unknown error"}`);
+      throw new Error(user.message || "Failed to fetch user");
+    }
+  } catch (error) {
+    console.error("User error:", error);
     throw error; 
   }
 }

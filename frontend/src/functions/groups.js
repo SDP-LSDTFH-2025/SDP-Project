722cba9b70a19c12c24b4a59b6bb308dd34bda2d
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-01 21:52:28 +0200

    feat: get all groups

---
+++ frontend/src/functions/groups.js
@@ -0,0 +1,22 @@
+export async function getAllGroups() {
+  try {
+    const SERVER = import.meta.env.VITE_PROD_SERVER || import.meta.env.VITE_DEV_SERVER;
+    let res = await fetch(`${SERVER}/api/v1/study_groups`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    let groups = await res.json();
+
+    if (groups.success) {
+      console.log("API Response:", groups);
+      return groups["data"];
+    } else {
+      console.error(`Failed to get groups: ${groups.message || "Unknown error"}`);
+      throw new Error(groups.message || "Failed to fetch groups");
+    }
+  } catch (error) {
+    console.error("Group error:", error);
+    throw error; 
+  }
+}

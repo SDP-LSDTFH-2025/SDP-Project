b998e36ed1643657d14873130d8414dd2ebea3e2
Lebo Kharafu <149167951+Lebo-Kharafu@users.noreply.github.com>
2025-09-01 20:10:46 +0200

    Update server.js removed lines

--- backend/server.js
+++ backend/server.js
@@ -141,8 +141,6 @@ async function startServer() {
     // Test database connection
     await sequelize.authenticate();
     console.log('✅ Database connection established successfully.');
-    //await sequelize.sync({alter:true}); Server is too slow to start
-    sequelize.sync();
     console.log('✅ Database synchronized successfully.');
     // Start server
     app.listen(PORT, () => {

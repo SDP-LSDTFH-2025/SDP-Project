7c8f9e5e6e1706fbef6028e026430fb867949917
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-21 11:09:24 +0200

    fix: Fixed Checking Test

--- backend/tests/api/check.test.js
+++ backend/tests/api/check.test.js
@@ -1,13 +1,11 @@
-import { describe, it, expect } from 'vitest'
-import request from 'supertest';
-import server from '../../server';
+import { describe, it, expect } from "vitest";
+import request from "supertest";
+import server from "../../server";
 
-describe('GET /api/v1', () => {
-    it('should return API INFO', async () => {
-      const response = await request(server)
-        .get(`/api/v1`)
-        .expect(200)
-      expect(response.body).toHaveProperty('message', "SDP Project API")
-    })
-
-  })
+describe("GET /api/v1", () => {
+	it("should return API INFO", async () => {
+		const response = await request(server).get(`/api/v1`).expect(200);
+		expect(response.body).toHaveProperty("message", "SDP Project API");
+    
+	});
+});

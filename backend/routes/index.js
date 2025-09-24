79ccf8f84873e3d9692503ffa167c86fb02751fa
trustgodm <makopoletrustgod.3@gmail.com>
2025-09-24 15:42:49 +0200

    feat: Added endpoints for getting messages by chatid. Fixed Scokets to be readable and polished other apis

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -14,6 +14,7 @@ const resourcesRoutes = require('./Resources')
 const resourcethreadsRoutes = require('./Resource_threads');
 const liked_Routes = require('./Likes');
 const publicApiRoutes = require('./PublicApi');
+const privateChatsRoutes = require('./PrivateChats');
 
 
 // API Documentation
@@ -55,7 +56,8 @@ router.get('/', (req, res) => {
       resources: '/resources',
       resource_threads: '/resource_threads',
       likes: '/likes/:id',
-      public: '/public'
+      public: '/public',
+      private_chats: '/private-chats'
 
     }
   });
@@ -74,4 +76,5 @@ router.use('/resources', resourcesRoutes);
 router.use('/resource_threads', resourcethreadsRoutes);
 router.use('/likes', liked_Routes);
 router.use('/public', publicApiRoutes);
+router.use('/private-chats', privateChatsRoutes);
 module.exports = router; 

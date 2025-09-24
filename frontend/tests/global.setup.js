46f325b209d68cdeaf28efeeba748ae8d73ce568
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-25 01:35:46 +0200

    bug: rewrite the global test script

--- frontend/tests/global.setup.js
+++ frontend/tests/global.setup.js
@@ -1,56 +1,59 @@
-import { chromium } from "playwright-extra";
+import { addExtra } from "playwright-extra";
+import { chromium as vanillaChromium } from "playwright";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import path from 'path';
+
+import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const stateFile = await path.join(__dirname, './state/storageState.json');
+const stateFile = await path.join(__dirname, "./state/storageState.json");
 export default async () => {
-  
-  chromium.use(StealthPlugin());
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  context.setDefaultTimeout(60000); // 60 seconds
-  const page = await context.newPage();
+	const chromium = addExtra(vanillaChromium);
 
-  const email = process.env.GOOGLE_TEST_EMAIL;
-  const password = process.env.GOOGLE_TEST_PASSWORD;
+	chromium.use(StealthPlugin());
+	const browser = await chromium.launch({ headless: false });
+	const context = await browser.newContext();
 
-  if (!email || !password) {
-    console.warn("Google test credentials not found");
-    await browser.close();
-    return;
-  }
+	context.setDefaultTimeout(60000);
+	const page = await context.newPage();
 
-  await page.goto('http://localhost:5173/');
-  await page.waitForURL("http://localhost:5173/");
-  await page.click('button:has-text("Log In")');
-  await page.waitForURL("http://localhost:5173/login");
-  await page
-    .getByRole('button', { name: 'Sign in with Google' })
-    .first()
-    .click();
+	const email = process.env.GOOGLE_TEST_EMAIL;
+	const password = process.env.GOOGLE_TEST_PASSWORD;
 
-  const [popup] = await Promise.all([page.waitForEvent('popup')]);
-  await popup.waitForLoadState();
+	if (!email || !password) {
+		console.warn("Google test credentials not found");
+		await browser.close();
+		return;
+	}
 
-  await popup.fill('input[type="email"]', email,{ timeout: 45000 });
-  await popup.click('button:has-text("Next")');
+	await page.goto("http://localhost:5173/");
+	await page.waitForURL("http://localhost:5173/");
+	await page.click('button:has-text("Log In")');
+	await page.waitForURL("http://localhost:5173/login");
+	await page
+		.getByRole("button", { name: "Sign in with Google" })
+		.first()
+		.click();
 
-  await popup.fill('input[type="password"]', password,{ timeout: 45000 });
-  await popup.click('button:has-text("Next")');
+	const [popup] = await Promise.all([page.waitForEvent("popup")]);
+	await popup.waitForLoadState();
 
-  const allowButton = popup.locator('button:has-text("Allow")');
-  if (await allowButton.isVisible({ timeout: 5000 })) {
-    await allowButton.click();
-  }
+	await popup.fill('input[type="email"]', email, { timeout: 60000 });
+	await popup.click('button:has-text("Next")');
 
-  await page.waitForURL("http://localhost:5173/home");
+	await popup.fill('input[type="password"]', password, { timeout: 60000 });
+	await popup.click('button:has-text("Next")');
 
-  await context.storageState({ path: stateFile });
+	const allowButton = popup.locator('button:has-text("Allow")');
+	if (await allowButton.isVisible({ timeout: 60000 })) {
+		await allowButton.click();
+	}
 
-  await browser.close();
+	await page.waitForURL("http://localhost:5173/home");
+
+	await context.storageState({ path: stateFile });
+
+	await browser.close();
 };
-

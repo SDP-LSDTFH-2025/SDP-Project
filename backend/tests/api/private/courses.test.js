5d9aefd31049720900239cfa68fbd8289fb435f8
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-27 23:19:06 +0200

    test: update the test

--- backend/tests/api/private/courses.test.js
+++ backend/tests/api/private/courses.test.js
@@ -2,17 +2,35 @@ import { describe, it, expect } from "vitest";
 import request from "supertest";
 import server from "../../../server";
 
-describe.skip("GET /api/v1/courses", () => {
+describe("GET /api/v1/courses", () => {
 	let response;
+	let success;
 	it("should return all courses", async () => {
-		response = await request(server).get(`/api/v1/courses`).expect(200);
-		expect(response.body).toHaveProperty("success",true);
+		response = await request(server).get(`/api/v1/courses`);
+		success = response.body.success;
+		expect(success).toBeTruthy();
 	});
 	
-	it.skip("should return first course", async () => {
+	it("should return first course", async () => {
 		if (response.body.data) {
-			const courseOne = response.body.data[0].id;
-			const response2 = await request(server).get(`/api/v1/courses/${courseOne}`).expect(200);
+			const courseOne = response.body.data[0].code;
+			const response2 = await request(server).get(`/api/v1/courses/search?search=${courseOne}`).expect(200);
+			expect(response2.body).toHaveProperty("data");
+		}
+	});
+
+	it("should search by name", async () => {
+		if (response.body.data) {
+			const courseOne = response.body.data[0].name;
+			const response2 = await request(server).get(`/api/v1/courses/code?code=${courseOne}`).expect(200);
+			expect(response2.body).toHaveProperty("data");
+		}
+	});
+
+	it("should search by course code", async () => {
+		if (response.body.data) {
+			const courseOne = response.body.data[0].code;
+			const response2 = await request(server).get(`/api/v1/courses/code?code=${courseOne}`).expect(200);
 			expect(response2.body).toHaveProperty("data");
 		}
 	});

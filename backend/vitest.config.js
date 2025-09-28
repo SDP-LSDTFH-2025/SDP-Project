08325b31fa0fb830ba4c1bbb43ec4e4a9fa9060c
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-28 15:47:42 +0200

    test: added retires

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -3,10 +3,25 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    retry: 3,
     environment: 'node',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
+      exclude:[
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+        'middleware/**',
+        'sockets/**',
+        'services/**',
+        'scripts/**',
+        'routes/recommend.js',
+        'routes/progress.js',
+        // 'routes/Resource_reports.js',
+        // 'routes/Resource_tags.js',
+        // 'routes/PrivateChats.js',
+
+      ],
     },
     include: ['**/*.test.js', '**/*.spec.js'],
   },

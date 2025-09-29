53c7c89f27b1034992a798f201de5efa2edc1643
Lebo-Kharafu <Kharafu23@gmail.com>
2025-09-29 23:42:24 +0200

    test: current progress

---
+++ frontend/tests/chatSystem.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Home Page Shows Correctly", () => {
+	test("Check if all needed things show", async ({ page }) => {
+		await page.goto('http://localhost:5173/home');
+		await expect(page.getByRole('button').filter({ hasText: /^$/ }).first()).toBeVisible();
+		await page.getByRole('button').filter({ hasText: /^$/ }).first().click();
+		await page.waitForURL('http://localhost:5173/messages');
+		await expect(page.getByRole('heading', { name: 'Chats' })).toBeVisible();
+		await expect(page.getByText('DMDimpho Matea')).toBeVisible();
+		await page.getByText('DMDimpho Matea').click();
+		await expect(page.locator('h2').filter({ hasText: 'Dimpho Matea' })).toBeVisible();
+
+	});
+});
---
+++ frontend/tests/profileSettings.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+	await page.goto('http://localhost:5173/home');
+	await expect(page.getByRole('button').filter({ hasText: /^$/ }).nth(2)).toBeVisible();
+	await page.getByRole('button').filter({ hasText: /^$/ }).nth(2).click();
+	await expect(page.getByRole('button', { name: 'Edit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Courses' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+	await expect(page.getByText('Role:')).toBeVisible();
+	await expect(page.getByText('Year of Study:')).toBeVisible();
+	await expect(page.getByText('Last Login:')).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Interests' })).toBeVisible();
+
+  	await expect(page.getByRole('button').filter({ hasText: /^$/ }).nth(2)).toBeVisible();
+	await page.getByRole('button').filter({ hasText: /^$/ }).nth(2).click();
+
+	await page.getByRole('button', { name: 'Edit' }).click();
+	await expect(page.getByText('Username')).toBeVisible();
+	await expect(page.getByText('Course', { exact: true })).toBeVisible();
+	await expect(page.getByText('Institution')).toBeVisible();
+	await expect(page.getByText('Faculty')).toBeVisible();
+	await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
+	await expect(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
+});

7a0278985c191b48ef858d33d97f43a81f9029dc
Lebo-Kharafu <Kharafu23@gmail.com>
2025-10-19 23:58:39 +0200

    feat: I am really disappointed by teams who messed up the ui last minutes and we were forced to restart test not frontend test sit at a low percentage

--- frontend/tests/007-studyBuddies.spec.js
+++ frontend/tests/007-studyBuddies.spec.js
@@ -1,14 +1,32 @@
 import { test, expect } from "playwright-test-coverage";
 
-test.describe("Home Page Shows Correctly", () => {
+test.describe("StudyBuddy Page Shows Correctly", () => {
 	test("Check if all needed things show", async ({ page }) => {
 		await page.goto('http://localhost:5174/home');
-		await expect(page.getByRole('heading', { name: 'StudyBuddy' })).toBeVisible();
-		await expect(page.getByText('NavigationResource')).toBeVisible();
-		await page.getByRole('heading', { name: 'Study Buddies' }).click();
-		await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
-		await expect(page.getByText('Resources Upload')).toBeVisible();
-		await expect(page.locator('div').filter({ hasText: 'Upcoming' }).nth(2)).toBeVisible();
-		await expect(page.getByRole('heading', { name: 'Group Studies' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Resource Feed' })).toBeVisible();
+		await page.getByRole('button', { name: 'Resource Feed' }).click();
+		await expect(page.getByRole('heading', { name: 'Resources' })).toBeVisible();
+		await expect(page.getByRole('button', { name: 'Share' })).toBeVisible();
+		await expect(page.locator('.file-preview').first()).toBeVisible();
+		await expect(page.getByRole('textbox', { name: 'Search resource by Title,' })).toBeVisible();
+		await expect(page.locator('.download-btn').first()).toBeVisible();
+		await expect(page.locator('.comment-input > button').first()).toBeVisible();
+		await expect(page.locator('.file-actions').first()).toBeVisible();
+		await page.locator('.lucide.lucide-heart').first().click();
+		await page.locator('.lucide.lucide-heart').first().click();
+		await page.locator('span:nth-child(2) > .lucide').first().click();
+		await page.locator('span:nth-child(3) > .lucide > line:nth-child(5)').first().click();
+		await page.locator('.comment-input > input').first().click();
+		await expect(page.getByRole('button', { name: 'Study Buddies' })).toBeVisible();
+		await page.getByRole('button', { name: 'Study Buddies' }).click();
+		await expect(page.getByRole('heading', { name: 'My Buddies' })).toBeVisible();
+		await expect(page.getByRole('textbox', { name: 'Search friends...' })).toBeVisible();
+		await expect(page.locator('div').filter({ hasText: /^DMDimpho Matea@Dimpho_MateaComputer • MIT FriendOnline Message Profile$/ }).getByRole('paragraph')).toBeVisible();
+		await page.getByRole('button', { name: 'Profile' }).first().click();
+		await expect(page.getByRole('heading', { name: 'Courses' })).toBeVisible();
+		await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+		await expect(page.getByRole('heading', { name: 'Interests' })).toBeVisible();
+		await expect(page.getByText('Role')).toBeVisible();
+
 	});
 });
--- frontend/tests/012-messages.spec.js
+++ frontend/tests/012-messages.spec.js
@@ -1,26 +1,12 @@
 import { test, expect } from "playwright-test-coverage";
 
 test.describe("Home Page Shows Correctly", () => {
+
 test('test', async ({ page }) => {
-	  test.setTimeout(240000); 
-
-  await expect(page.getByRole('button', { name: 'Messages' })).toBeVisible();
-  await page.getByRole('button', { name: 'Messages' }).click();
-
-  await expect(page.getByRole('button', { name: 'Progress' })).toBeVisible();
-  await page.getByRole('button', { name: 'Messages' }).click();
-  await page.waitForTimeout(30000); 
-  await page.getByRole('button', { name: 'Notifications' }).nth(1).click();
-  await page.waitForTimeout(30000); 
-  await page.getByRole('button', { name: 'Messages' }).click();
-
-  await expect(page.getByRole('heading', { name: 'Messages' })).toBeVisible();
+  await page.goto('http://localhost:5174/home');
+  await expect(page.getByRole('button').filter({ hasText: /^$/ }).first()).toBeVisible();
+  await page.getByRole('button').filter({ hasText: /^$/ }).first().click();
   await expect(page.getByRole('button', { name: 'Back to Home' })).toBeVisible();
-  await expect(page.locator('div').filter({ hasText: /^Chats$/ }).nth(1)).toBeVisible();
-  await expect(page.locator('.chat-window')).toBeVisible();
-  await page.getByText('Dimpho Matea22:').click();
-  await expect(page.getByRole('button', { name: 'Start video call' })).toBeVisible();
-  await expect(page.locator('.whatsapp-input-container')).toBeVisible();
-  await page.getByRole('button', { name: 'Back to Home' }).click();
+  await expect(page.locator('h1')).toBeVisible();
 });
 });

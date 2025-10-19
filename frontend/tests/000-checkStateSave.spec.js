06d89e0211a51c0e5644e320c9a14ce2f5beea08
Lebo-Kharafu <Kharafu23@gmail.com>
2025-10-19 12:05:50 +0200

    feat: first few tests

--- frontend/tests/000-checkStateSave.spec.js
+++ frontend/tests/000-checkStateSave.spec.js
@@ -11,6 +11,6 @@ import { test, expect } from "playwright-test-coverage";
 test.describe("Home Saved state", () => {
 	test("Check if state works", async ({ page }) => {
 		await page.goto("http://localhost:5174/home");
-		await expect(page.getByRole("button", { name: "logout" })).toBeVisible();
+		await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
 	});
 });
--- frontend/tests/postLogin.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "playwright-test-coverage";
-
-test.describe("Home Page Shows Correctly", () => {
-	test("Check if all needed things show", async ({ page }) => {
-		await page.goto('http://localhost:5174/home');
-
-	});
-});
